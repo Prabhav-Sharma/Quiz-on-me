fd2b3e09538b91611aed8dcb221bc5561e397c15
Prabhav-Sharma <prabhav.ps8@gmail.com>
2022-01-10 15:18:08 +0530

    Feat: ES6
    Code updated to ES6

--- index.js
+++ index.js
@@ -1,35 +1,38 @@
-var readline= require('readline-sync');
+//ES6 Version Code 
 
-var name= readline.question("Can I have your name? ");
+const readline= require('readline-sync');
+
+const name= readline.question("Can I have your name? ");
 
 
-var questionOne ={
+const questionOne ={
   question:"Who is my favorite superhero? ",
   answer:"Iron Man"
+
 }
 
-var questionTwo ={
+const questionTwo ={
   question:"Which is my favourite sad song? ", 
   answer:"Channa Mereya"
 }
 
-var questionThree ={
+const questionThree ={
   question:"Which is my favourite food? ", 
   answer:"Chicken"
 }
-var questionFour ={
+const questionFour ={
   question:"Which is my favourite fast-food chain? ", 
   answer:"Taco Bell"
 }
-var questionFive ={
+const questionFive ={
   question:"Which is my favourite kind of pet? ", 
   answer:"Dogs"
 }
 
-var score=0;
+let score=0;
 
-function play(question, answer){
-var userAnswer= readline.question(question);
+const play=(question, answer)=>{
+let userAnswer= readline.question(question);
 
 if(userAnswer.toUpperCase()===answer.toUpperCase()){
   console.log('You are absolutely Right!')
@@ -37,36 +40,36 @@ if(userAnswer.toUpperCase()===answer.toUpperCase()){
 }else{
   console.log("That's Incorrect Boohoo");
 }
-console.log("Current Score: " + score);
+console.log(`Current Score: ${score}`);
 console.log("----------------------");
 }
 
-var questions=[questionOne,questionTwo,questionThree,questionFour,questionFive];
+let questions=[questionOne,questionTwo,questionThree,questionFour,questionFive];
 
-for(var i=0; i<questions.length; i++){
+for(let i=0; i<questions.length; i++){
   play(questions[i].question,questions[i].answer);
 }
 
 console.log("Game Over");
 console.log( name + "'s score: " + score);
 
-var umme={
+let umme={
   name:"Umme",
   score:5
 };
 
-var prabhav={
+let prabhav={
   name:"Prabhav",
   score:4
 };
-var user={
+let user={
   name: name,
   score:score
 };
 
-var scores=[umme, prabhav, user];
+let scores=[umme, prabhav, user];
 
-for(var i=0; i<scores.length; i++){
+for(let i=0; i<scores.length; i++){
   console.log(scores[i])
 }
 if(score >= umme.score){
